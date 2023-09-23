154a5140eb41e311e39e83c423483e5e772a1c90
vella <vadmir04@gmail.com>
2023-09-24 01:36:31 +0200

    Add UrlMassage.java

---
+++ src/main/java/com/vella/URLShortenerManagmenetService/Model/UrlMessage.java
@@ -0,0 +1,31 @@
+package com.vella.URLShortenerManagmenetService.Model;
+
+
+import com.google.gson.Gson;
+import jakarta.persistence.Entity;
+import lombok.*;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Getter
+@Setter
+public class UrlMessage {
+
+    private Long id;
+
+    private String realURL;
+
+    private String shortURL;
+
+    private Action action;
+
+    public UrlMessage(Url url, Action action) {
+        this.shortURL = url.getShortURL();
+        this.realURL = url.getRealURL();
+        this.action = action;
+    }
+}
+
+
