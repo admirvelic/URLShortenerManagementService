fe2187be6a2ba67d6cb41cdf4de2c8ec3fafbce2
vella <vadmir04@gmail.com>
2023-09-24 13:31:45 +0200

    Format UrlShortenerManagementServiceApplication.java.java

---
+++ src/main/java/com/vella/URLShortenerManagementService/UrlShortenerManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.vella.URLShortenerManagementService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrlShortenerManagementServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrlShortenerManagementServiceApplication.class, args);
+    }
+
+}
