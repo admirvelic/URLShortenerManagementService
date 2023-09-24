2f3be9beb68078bf0153174912e533cf8f69b57b
vella <vadmir04@gmail.com>
2023-09-24 13:29:23 +0200

    Format ProducerService.java

--- src/main/java/com/vella/URLShortenerManagementService/service/ProducerService.java
+++ src/main/java/com/vella/URLShortenerManagementService/service/ProducerService.java
@@ -1,4 +1,4 @@
-package com.vella.URLShortenerManagmenetService.Service;
+package com.vella.URLShortenerManagementService.service;
 
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
