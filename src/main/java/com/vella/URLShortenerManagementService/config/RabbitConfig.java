2906ed48f4a7eb08037bb9c11f93b40614f17b9e
vella <vadmir04@gmail.com>
2023-09-24 13:29:37 +0200

    Format RabbitConfig.java

--- src/main/java/com/vella/URLShortenerManagementService/config/RabbitConfig.java
+++ src/main/java/com/vella/URLShortenerManagementService/config/RabbitConfig.java
@@ -1,4 +1,4 @@
-package com.vella.URLShortenerManagmenetService.Config;
+package com.vella.URLShortenerManagementService.config;
 
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
