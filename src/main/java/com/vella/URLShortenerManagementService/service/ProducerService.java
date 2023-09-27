6125d47a13bc09c612a71dc8aa6f22ee7ffeff48
vella <vadmir04@gmail.com>
2023-09-27 13:02:40 +0200

    Fix ProducerService.java

--- src/main/java/com/vella/URLShortenerManagementService/service/ProducerService.java
+++ src/main/java/com/vella/URLShortenerManagementService/service/ProducerService.java
@@ -1,10 +1,12 @@
 package com.vella.URLShortenerManagementService.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
+@Slf4j
 @Service
 public class ProducerService {
 
@@ -16,5 +18,6 @@ public class ProducerService {
 
     public void sendMessage(String message) {
         rabbitTemplate.convertAndSend(queueName, message);
+        log.info("Sent <" + message + ">");
     }
 }
